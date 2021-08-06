370884197ce506e42967e00e9bbeb56f79e0f4da
María José Baquiax <mariabaquiax201831422@cunoc.edu.gt>
2021-08-05 22:48:42 -0600

    intentando arreglar el split

--- src/practicalfydp/identificador.java
+++ src/practicalfydp/identificador.java
@@ -12,10 +12,43 @@ public class identificador {
             for (int j = 0; j < lineas[i].length(); j++) {      //caracter
                 int actual, siguiente = -1;
                 actual = lineas[i].codePointAt(j);      //Devuelve codigo ASCII en la posición index pasada por parámetro
+                if (est == 0) {
+                    est = transicion(actual);
+                }
                 
+                try {
+                    siguiente = lineas[i].codePointAt(j+1);     //ascii
+                } catch (Exception e) {
+                    
+                }
+                
+                switch(est){
+                    case 1: 
+                        estructura += lineas[i].charAt(j);
+                        //minusculas ASCII 97-122
+                        //mayusculas ASCII 65-90
+                        if ((siguiente >= 97 && siguiente <= 122) || (siguiente >= 65 && siguiente <= 90)) {
+                            est = 1;
+                        }
+                        else {
+                            tokenNum = 1;
+                            tipo = "     CADENA     ";
+                            est = 0;
+                        }
+                        
+                }
             }
         }
         
+    }
+    
+    public  int transicion(int numero){
+        //minusculas ASCII 97-122
+        //mayusculas ASCII 65-90
+        if ((numero >= 97 && numero <= 122) || (numero >= 65 && numero <= 90)) {
+            return 1;
+        }
+        return 0;
     }
     
     //separar cadena`x
